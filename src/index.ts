91bb514bc2ce42f1795e87cd35da716c1d24f926
mhvue <mhvue4@gmail.com>
2021-09-10 19:14:28 -0500

    connected to MongoDB successfully

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import express from "express";
 import {json} from "body-parser";
 import { todoRouter } from "./routes/todo";
+import mongoose from "mongoose";
 
 const app = express ();
 app.use(json());
 //register our route
 app.use(todoRouter);
 
-//listening for our server 
-app.listen(3000, () => {
-    console.log("server is listening to port 3000")
-});
+//connect to mongoDB
+mongoose.connect("mongodb://localhost:27017/todos")
+.then(() => {
+    console.log("mongoDB connected successfully!")
+    //listening for our server 
+    app.listen(3000, () => {
+        console.log("server is listening to port 3000")
+    });
+}).catch((error) => {
+    console.log("error", error)
+})
